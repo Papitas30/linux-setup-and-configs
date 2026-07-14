955d7d69e907b1df0247bcce04f44c297b888011
Diego Diaz <papitas30gameryt@gmail.com>
2026-07-14 12:00:55 -0500

    zshrc

--- config/.config/Thunar/accels.scm
+++ config/.config/Thunar/accels.scm
@@ -108,6 +108,7 @@
 ; (gtk_accel_path "<Actions>/ThunarActionManager/trash-delete-2" "KP_Delete")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/back-alt1" "BackSpace")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/create-folder" "<Primary><Shift>n")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/27ec97b47793015cf11b0e43d5ceac03" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/open-home" "<Alt>Home")
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-focused-split-view-pane" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/show-hidden" "<Primary>h")
